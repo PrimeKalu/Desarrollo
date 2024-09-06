85acab341c1b7bac7009086894d17386a2afbfd0
PrimeKalu <serna.daniel@uabc.edu.mx>
2024-09-06 15:56:16 -0700

    Prueba
    Es una prueba

--- entidad/src/main/java/mx/desarrollo/entidad/Alumno.java
+++ entidad/src/main/java/mx/desarrollo/entidad/Alumno.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-/* Este es mi primer cambioooo*/
+/* Este es mi primer cambiooooooo*/
 package mx.desarrollo.entidad;
 
 import java.io.Serializable;
