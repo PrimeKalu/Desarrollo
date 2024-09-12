a543c449c06ff07cb0d410a341368a62d8940e39
GustavoDanielBC <gustavo.barrios@uabc.edu.mx>
2024-09-12 15:19:14 -0700

    Cambios
    Se hicieron cambios en capa entidad

---
+++ entidad/src/main/java/mx/desarrollo/entidad/Login.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.desarrollo.entidad;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author usuario
+ */
+@Entity
+@Table(name = "login")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Login.findAll", query = "SELECT l FROM Login l")
+    , @NamedQuery(name = "Login.findByIdLogin", query = "SELECT l FROM Login l WHERE l.idLogin = :idLogin")
+    , @NamedQuery(name = "Login.findByPass", query = "SELECT l FROM Login l WHERE l.pass = :pass")})
+public class Login implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "idLogin")
+    private Integer idLogin;
+    @Basic(optional = false)
+    @Column(name = "pass")
+    private String pass;
+
+    public Login() {
+    }
+
+    public Login(Integer idLogin) {
+        this.idLogin = idLogin;
+    }
+
+    public Login(Integer idLogin, String pass) {
+        this.idLogin = idLogin;
+        this.pass = pass;
+    }
+
+    public Integer getIdLogin() {
+        return idLogin;
+    }
+
+    public void setIdLogin(Integer idLogin) {
+        this.idLogin = idLogin;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idLogin != null ? idLogin.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Login)) {
+            return false;
+        }
+        Login other = (Login) object;
+        if ((this.idLogin == null && other.idLogin != null) || (this.idLogin != null && !this.idLogin.equals(other.idLogin))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "mx.desarrollo.entidad.Login[ idLogin=" + idLogin + " ]";
+    }
+    
+}
